22d4373d7d16067eba043b2dcd435dfeb92171c7
KamilK <Kamil.Koszarny@pentacomp.pl>
2021-01-04 16:48:42 +0100

    merged with SceneBuilder mock

---
+++ src/Controller.java
@@ -0,0 +1,111 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField fullname;
+
+    @FXML
+    private ChoiceBox<?> proffesion;
+
+    @FXML
+    private ChoiceBox<?> race;
+
+    @FXML
+    private TextField age;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private TextField weight;
+
+    @FXML
+    private ChoiceBox<?> sign;
+
+    @FXML
+    private TextField religion;
+
+    @FXML
+    private TextField origin;
+
+    @FXML
+    private TextField appearance;
+
+    @FXML
+    private TextArea history;
+
+    @FXML
+    private TextField vim;
+
+    @FXML
+    private TextField strengthBase;
+
+    @FXML
+    private TextField strength;
+
+    @FXML
+    private TextField enduranceBase;
+
+    @FXML
+    private TextField endurance;
+
+    @FXML
+    private TextField formBase;
+
+    @FXML
+    private TextField form;
+
+    @FXML
+    private TextField efficiency;
+
+    @FXML
+    private TextField eyeBase;
+
+    @FXML
+    private TextField eye;
+
+    @FXML
+    private TextField armBase;
+
+    @FXML
+    private TextField arm;
+
+    @FXML
+    private TextField agilityBase;
+
+    @FXML
+    private TextField agility;
+
+    @FXML
+    private TextField intelligence;
+
+    @FXML
+    private TextField knowledgeBase;
+
+    @FXML
+    private TextField knowledge;
+
+    @FXML
+    private TextField focusBase;
+
+    @FXML
+    private TextField focus;
+
+    @FXML
+    private TextField charismaBase;
+
+    @FXML
+    private TextField charisma;
+
+    @FXML
+    private TextField luckPoints;
+
+    @FXML
+    private TextField experiencePoints;
+
+}
+
