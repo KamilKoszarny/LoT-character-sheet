fcad7cde5de1faae19a0de8a35562ec107ae490e
KamilK <Kamil.Koszarny@pentacomp.pl>
2021-01-04 19:58:25 +0100

    basic logic of attributes

--- src/controller/PlayerUpdater.java
+++ src/controller/PlayerUpdater.java
@@ -22,12 +22,18 @@ public class PlayerUpdater {
     }
 
     private static void initStatsUpdating() {
-        initStrengthListener();
-        initEnduranceListener();
-        initFormListener();
+        initStrengthBaseListener();
+        initEnduranceBaseListener();
+        initFormBaseListener();
+        initArmBaseListener();
+        initEyeBaseListener();
+        initAgilityBaseListener();
+        initKnowledgeBaseListener();
+        initFocusBaseListener();
+        initCharismaBaseListener();
     }
 
-    private static void initStrengthListener() {
+    private static void initStrengthBaseListener() {
         guiController.getStrengthBase().textProperty().addListener((observable, oldValue, newValue) -> {
             currentPlayer.setStrengthBase(Integer.parseInt(newValue));
             updateStrength();
@@ -35,7 +41,7 @@ public class PlayerUpdater {
         });
     }
 
-    private static void initEnduranceListener() {
+    private static void initEnduranceBaseListener() {
         guiController.getEnduranceBase().textProperty().addListener((observable, oldValue, newValue) -> {
             currentPlayer.setEnduranceBase(Integer.parseInt(newValue));
             updateEndurance();
@@ -43,11 +49,59 @@ public class PlayerUpdater {
         });
     }
 
-    private static void initFormListener() {
+    private static void initFormBaseListener() {
         guiController.getFormBase().textProperty().addListener((observable, oldValue, newValue) -> {
             currentPlayer.setFormBase(Integer.parseInt(newValue));
             updateForm();
             updateVim();
+        });
+    }
+
+    private static void initArmBaseListener() {
+        guiController.getArmBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setArmBase(Integer.parseInt(newValue));
+            updateArm();
+            updateEfficiency();
+        });
+    }
+
+    private static void initEyeBaseListener() {
+        guiController.getEyeBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setEyeBase(Integer.parseInt(newValue));
+            updateEye();
+            updateEfficiency();
+        });
+    }
+
+    private static void initAgilityBaseListener() {
+        guiController.getAgilityBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setAgilityBase(Integer.parseInt(newValue));
+            updateAgility();
+            updateEfficiency();
+        });
+    }
+
+    private static void initKnowledgeBaseListener() {
+        guiController.getKnowledgeBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setKnowledgeBase(Integer.parseInt(newValue));
+            updateKnowledge();
+            updateIntelligence();
+        });
+    }
+
+    private static void initFocusBaseListener() {
+        guiController.getFocusBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setFocusBase(Integer.parseInt(newValue));
+            updateFocus();
+            updateIntelligence();
+        });
+    }
+
+    private static void initCharismaBaseListener() {
+        guiController.getCharismaBase().textProperty().addListener((observable, oldValue, newValue) -> {
+            currentPlayer.setCharismaBase(Integer.parseInt(newValue));
+            updateCharisma();
+            updateIntelligence();
         });
     }
 
@@ -73,6 +127,54 @@ public class PlayerUpdater {
         int vim = StatsCalculator.calculateVim(currentPlayer);
         currentPlayer.setVim(vim);
         guiController.getVim().setText(Integer.toString(vim));
+    }
+
+    private static void updateArm() {
+        int arm = StatsCalculator.calculateArm(currentPlayer);
+        currentPlayer.setArm(arm);
+        guiController.getArm().setText(Integer.toString(arm));
+    }
+
+    private static void updateEye() {
+        int eye = StatsCalculator.calculateEye(currentPlayer);
+        currentPlayer.setEye(eye);
+        guiController.getEye().setText(Integer.toString(eye));
+    }
+
+    private static void updateAgility() {
+        int agility = StatsCalculator.calculateAgility(currentPlayer);
+        currentPlayer.setAgility(agility);
+        guiController.getAgility().setText(Integer.toString(agility));
+    }
+
+    private static void updateEfficiency() {
+        int efficiency = StatsCalculator.calculateEfficiency(currentPlayer);
+        currentPlayer.setEfficiency(efficiency);
+        guiController.getEfficiency().setText(Integer.toString(efficiency));
+    }
+
+    private static void updateKnowledge() {
+        int knowledge = StatsCalculator.calculateKnowledge(currentPlayer);
+        currentPlayer.setKnowledge(knowledge);
+        guiController.getKnowledge().setText(Integer.toString(knowledge));
+    }
+
+    private static void updateFocus() {
+        int focus = StatsCalculator.calculateFocus(currentPlayer);
+        currentPlayer.setFocus(focus);
+        guiController.getFocus().setText(Integer.toString(focus));
+    }
+
+    private static void updateCharisma() {
+        int charisma = StatsCalculator.calculateCharisma(currentPlayer);
+        currentPlayer.setCharisma(charisma);
+        guiController.getCharisma().setText(Integer.toString(charisma));
+    }
+
+    private static void updateIntelligence() {
+        int intelligence = StatsCalculator.calculateIntelligence(currentPlayer);
+        currentPlayer.setIntelligence(intelligence);
+        guiController.getIntelligence().setText(Integer.toString(intelligence));
     }
 
     public static void setGuiController(GuiController guiController) {
--- src/controller/StatsCalculator.java
+++ src/controller/StatsCalculator.java
@@ -4,8 +4,6 @@ import model.Player;
 
 public class StatsCalculator {
 
-    private static Player currentPlayer;
-
     protected static int calculateStrength(Player player) {
         int strength = player.getStrengthBase();
         return strength;
@@ -24,5 +22,45 @@ public class StatsCalculator {
     protected static int calculateVim(Player player) {
         int vim = (int) Math.round((player.getStrengthBase() + player.getEnduranceBase() + player.getFormBase()) / 3.);
         return vim;
+    }
+
+    protected static int calculateArm(Player player) {
+        int arm = player.getArmBase();
+        return arm;
+    }
+
+    protected static int calculateEye(Player player) {
+        int eye = player.getEyeBase();
+        return eye;
+    }
+
+    protected static int calculateAgility(Player player) {
+        int agility = player.getAgilityBase();
+        return agility;
+    }
+
+    protected static int calculateEfficiency(Player player) {
+        int efficiency = (int) Math.round((player.getArmBase() + player.getEyeBase() + player.getAgilityBase()) / 3.);
+        return efficiency;
+    }
+
+    protected static int calculateKnowledge(Player player) {
+        int knowledge = player.getKnowledgeBase();
+        return knowledge;
+    }
+
+    protected static int calculateFocus(Player player) {
+        int focus = player.getFocusBase();
+        return focus;
+    }
+
+    protected static int calculateCharisma(Player player) {
+        int charisma = player.getCharismaBase();
+        return charisma;
+    }
+
+    protected static int calculateIntelligence(Player player) {
+        int intelligence = (int) Math.round((player.getKnowledgeBase() + player.getFocusBase() + player.getCharismaBase()) / 3.);
+        return intelligence;
     }
 }
