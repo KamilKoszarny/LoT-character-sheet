a661015b65d63a8450ed394dce2179d4045fe548
KamilK <Kamil.Koszarny@pentacomp.pl>
2021-01-12 17:52:08 +0100

    .lotcs extension

--- src/controller/PlayerSaveLoader.java
+++ src/controller/PlayerSaveLoader.java
@@ -12,6 +12,7 @@ public class PlayerSaveLoader {
     private static Stage primaryStage;
     private static MenuItem saveAsButton;
     private static MenuItem openButton;
+    private static FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("Legacy of Tyrant Character Sheet", "*.lotcs");
 
     public static void init(Stage primaryStage, GuiController guiController) {
         PlayerSaveLoader.primaryStage = primaryStage;
@@ -24,6 +25,7 @@ public class PlayerSaveLoader {
     static void initOpenButton() {
         openButton.setOnAction(event -> {
             FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(extensionFilter);
             File file = fileChooser.showOpenDialog(primaryStage);
             if (file == null) return;
             try {
@@ -42,6 +44,7 @@ public class PlayerSaveLoader {
     static void initSaveAsButton() {
         saveAsButton.setOnAction(event -> {
             FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(extensionFilter);
             File file = fileChooser.showSaveDialog(primaryStage);
             if (file == null) return;
             try {
